c6b1f1fd0aa16f8206e303ff13c2cd0bee1e0787
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-01-25 12:53:45 +0100

    Update polosyceros.m

--- Examen lab TDS/polosyceros.m
+++ Examen lab TDS/polosyceros.m
@@ -2,4 +2,18 @@ close all,clc;clear;
 
 c5 = 0.5*exp((pi/4)*1i)
 
-c1 = sqrt(2)*exp((pi/4)*i)
+c1 = sqrt(2)*exp((pi/4)*i);
+
+
+x=[1 2 3 3 3 3 2 1];
+h=[1 2 3 2 1];
+
+y=cconv(x,h)
+
+
+x=[1 2 1]
+
+y=cconv(x,x,4)
+
+
+g = fft(y)
