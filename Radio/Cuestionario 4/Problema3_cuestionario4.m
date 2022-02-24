a351934aca4f02e8d79aab6e7d2ff45a1254d647
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-02-24 21:12:00 +0100

    continuacion código reflexion

--- Radio/Cuestionario 4/Problema3_cuestionario4.m
+++ Radio/Cuestionario 4/Problema3_cuestionario4.m
@@ -9,28 +9,33 @@ h2 = 300;
 Distancia = 35000;
 R0 =6370000;
 
+Permeavilidad_terreno = 70;
+Conductividad=5;
+
+
 K = 4/3;
 termino1 = K*R0+h1;
 termino2= K*R0;
 termino3 = K*R0+h2;
 termino4 = K*R0;
-Dmax = sqrt(termino1^2 - termino2^2)+sqrt(termino3^2 - termino4^2)
+Dmax = sqrt(termino1^2 - termino2^2)+sqrt(termino3^2 - termino4^2);
 
 if(Distancia<0.1*Dmax) 
     %Código ejecutado si tierra plana
     Caso = "Tierra plana"
 else
     %Código ejecutado si tierra curva
-    Caso = "Tierra curva"
+    Caso = "Tierra curva";
 end
 
 
-f=6000000;
+f=6e9;
 c=3e8;
 lambda = c/f;
-Factor_rugosidad = 0.1;
+Rho = 0.1;
 
-P = (2/sqrt(3))*sqrt((K*R0*(h1+h2)+(Distancia^2)/4)); 
+
+P = (2/3^0.5)*(((K*R0*(h1+h2)+(Distancia^2)/4)))^0.5; 
 
 if(h1>h2)
     Thau = acos((2*K*R0*(h1-h2)*Distancia)/P^3);
@@ -46,25 +51,33 @@ H2 = h2 - (d2^2)/(2*K*R0);
 H1 = h1 - (d1^2)/(2*K*R0);
 
 Phi = atan(H1/d1);
-Phi_lim = (5400/(f/1000))^(1/3);
-if(Phi<0.1)
- R = -1;
-end
+Phi_lim = (1/1000)*(5400/(f/1000))^(1/3);
+
+Epsilon0 = Permeavilidad_terreno -1i*60*Conductividad*lambda;
+numerador   = Epsilon0*sin(Phi) - sqrt(Epsilon0-(cos(Phi)*cos(Phi)));
+denominator = Epsilon0*sin(Phi) + sqrt(Epsilon0-(cos(Phi)*cos(Phi)));
+Rv =numerador/denominator;
+% Programar para que vaya solito poe PH/PV
+
 
 if(Phi>=Phi_lim)
- %Código ejecutado si hay reflexión -> MCTE
+ %Código ejecutado si hay reflexión -> MTC
     "Hay pérdidas por reflexión"
-    Divergencia = (1 + (5/(16*K)*((d2*d1^2)/(Distancia*H1))))^(-1/2); 
-    Rugosidad = (4*pi*Factor_rugosidad*sin(Phi))/lambda
+    Rugosidad = 4*pi*Rho*sin(Phi)/lambda;
+    
     if(Rugosidad>0.3)
         %Las pérdidas por refelxión son cero;
-        Ldif = 0;
+        Lref = 0;
     else
-        Divergencia = (1 + (5/(16*K)*((d2*d1^2)/(Distancia*H1))))^(-1/2); 
-        Rugosidad = (4*pi*Factor_rugosidad*sin(Phi))/lambda;
+        Dcaminos =sqrt( Distancia*Distancia + abs(H1+H2)^2 ) - sqrt( Distancia*Distancia + abs(H1-H2)^2 );
+        
+        Divergencia = (1 + (5/(16*K)*((d2*d1*d1)/(Distancia*H1))))^(-1/2);
+        Refectivo = Rv*Divergencia*exp(-((Rugosidad*Rugosidad)/2))
+        exponente = (-1i*(((2*pi)/lambda))*Dcaminos);
+        Lref_dB   = -20*log10(norm( 1 + (Refectivo*exp(exponente)) )) 
     end
-    Re = Distancia*Divergencia*Rugosidad;
+    
 else
- %Código ejecutado si hay difracción
+ %Código ejecutado si hay difracción -> MDTE
     "Hay pérdidas por difracción"
 end
