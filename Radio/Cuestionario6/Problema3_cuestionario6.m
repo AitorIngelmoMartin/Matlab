3b991a98936f5c209e1714c0358dc8964840a1da
Aitor Ingelmo <81867849+AitorIngelmoMartin@users.noreply.github.com>
2022-03-12 17:43:38 +0100

    Correcciones cuestionario 6

--- Radio/Cuestionario6/Problema3_cuestionario6.m
+++ Radio/Cuestionario6/Problema3_cuestionario6.m
@@ -1,56 +1,81 @@
 clear;close all;clc;
 
 % **************** DESVANECIMIENTO POR LLUVIA *****************
+<<<<<<< Updated upstream
 f    = 14;
 R0   = 6370000;
 K    = 0.0374;
 Re   = K*R0;
 hs   = 368/1000; %Km
 h0   = 2.33; %Km
+=======
+f  = 14e9;
+c  = 3e8;
+R0 = 6370000;
+K  = 0.0374;
+Re = K*R0;
+hs = 368/1000;
+h0 = 2.33; %Km
+>>>>>>> Stashed changes
 Lat  = 36.71;
 Long = 5.7;
 Angulo_elev = 27.3; %Grados
 Distancia   = 38920; % Km
-
+f           = 14;
 G_dBi   = 27;
 G_dB    = G_dBi - 2.14;
 Ptx_dBw = 5;
 R_001   = 38.89;% mm/H
 Alpha   = 1.1396;
 hr      = (h0+0.36) %Km
+<<<<<<< Updated upstream
 MD_dB   = 34;
+=======
+>>>>>>> Stashed changes
 
-Lespecifica_lluvia = K * R_001  % dB/Km
+Lespecifica_lluvia = K * R_001^Alpha  % dB/Km
 
 if(Angulo_elev>=5)
    Ls = (hr-hs)/sind(Angulo_elev) % metros 
 else
+<<<<<<< Updated upstream
    Ls = (2*(hr-hs)/(sqrt((sin(Angulo_elev))^2)+ (2*(hr-hs))/Re) +sind(Angulo_elev));
 end
 
 Lg       = Ls*cos(Angulo_elev)
 r_001    = 1/(1 + 0.78*sqrt((Lg*Lespecifica_lluvia)/f) - 0.38*(1-exp(-2*Lg)))
 arcotang = atan((1000*hr - hs)/(Lg*r_001));
+=======
+   Ls = (2*(ht-hs)/(sqrt((sind(Angulo_elev))^2)+ (2*(hr-hs))/Re) +sin(Angulo_elev))
+end
+
+Lg       = Ls*cosd(Angulo_elev)
+r_001    = 1/(1 + 0.78*sqrt((Lg*Lespecifica_lluvia)/(f)) - 0.38*(1-exp(-2*Lg)))
+arcotang = atand((hr-hs)/(Lg*r_001));
+>>>>>>> Stashed changes
 
 if(arcotang >Angulo_elev)
-    Lr = (Lg*R_001)/cos(Angulo_elev) 
+    Lr = (Lg*r_001)/(cosd(Angulo_elev)) 
 else
+<<<<<<< Updated upstream
     Lr = (1000*hr - hs)/sin(Angulo_elev)
+=======
+    Lr = (hr -hs)/(sind(Angulo_elev))
+>>>>>>> Stashed changes
 end
 
 Lat = abs(Lat);
 
 if(Lat<36)
-    Epsilno  = 36 - Lat 
+    Epsilno  = 36 - Lat; 
 else
-    Epsilon =0; 
+    Epsilon  =0; 
 end
 
 termino3 = (31*(1-exp(-(Angulo_elev)/(1+Epsilon))*sqrt(Lr*Lespecifica_lluvia)))/(f*f)
-
-V_001 = 1/(1+sqrt(sin(Angulo_elev))*(termino3-0.45))
-Deff  = (Lr*V_001)/1000 %Km
-F_001 = Lespecifica_lluvia*Deff %dB
+V_001    = 1/(1+sqrt(sind(Angulo_elev))*(termino3-0.45))
+Deff     = (Lr*V_001) %Km
+F_001    = Lespecifica_lluvia*Deff %dB
 
 
 
--- Radio/Cuestionario7/problema1_cuestionario7.m
+++ Radio/Cuestionario7/problema1_cuestionario7.m
@@ -1,36 +1,28 @@
 clear;close all;clc;
 
 % **************** DESVANECIMIENTO POR LLUVIA *****************
-f       = 38e9;
-c       = 3e8;
-R0      = 6370000;
-K       = 0.3884;
-MD_dB   = 34;
-MD      = 10^(MD_dB/10);
-f  = 38e9;
+f  = 18e9;
 c  = 3e8;
 R0 = 6370000;
 K  = 0.3884;
 lambda    = c/f;
-Distancia = 15; % Km
-f       = 38;
+Distancia = 16; % Km
+
 G_dBi   = 27;
 G_dB    = G_dBi - 2.14;
 Ptx_dBw = 5;
 R_001   = 24.42;% mm/Km
 Alpha   = 0.8552;
 
-lambda    = c/f;
-Distancia = 15; % Km
-f         = 38;
+MargenDinamico_atenuador_dB = 34;
 
 Lespecifica_lluvia = K *(R_001^Alpha)  % dB/Km
 
-termino1 = 0.477*(Distancia^0.633)*(R_001^(0.073*Alpha))*(f^(0.123));
+termino1=0.477*(Distancia^0.633)*(R_001^(0.073*Alpha))*(f^(0.123));
 termino2 = 10.579*(1-exp(-0.024*Distancia));
-Deff     = (Distancia)/(termino1-termino2) %Km
+Deff = (Distancia)/(termino1-termino2) %Km
 
-F_001    = Lespecifica_lluvia* Deff % dB
+F_001 = Lespecifica_lluvia* Deff % dB
 % Almenos en una hora al año, la lluvia va a probocar una atenuación
 % mayor que F_001 en dB.
 
@@ -42,14 +34,9 @@ end
 
 C1 = (0.07^C0)  * (0.12^(1-C0));
 C2 = (0.855*C0) + 0.5446*(1-C0);
-C3 = (0.139*C0) + 0.043* (1-C0);
+C3 = (0.139*C0) + 0.043*(1-C0) ;
 
 % Fq = F_001*C1*(0.01^(-(C2+C3*log10(0.01))));
-logaritmo = log10(MD/F_001*C1);
-
-soluciones_x =  [( -C2 + sqrt( C2*C2 -4*logaritmo*C3 ) )/(2*C3),( -C2 - sqrt( C2*C2 -4*logaritmo*C3 ) )/(2*C3)];
-x =max(soluciones_x);
-q = 10^x
 
 Lbf_dB = 20*log10((4*pi*Distancia*Distancia)/lambda);
 
